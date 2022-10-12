a03d3aa55cf301f7b86d8b1e13309b391d853b89
Denis Gichana <denisgichana162@gmail.com>
2022-10-12 14:07:33 +0300

    connect to frontend through socket io

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User connected ${socket.id}`);
+
+  socket.on("join-room", (data) => {
+    socket.join(data);
+    console.log(`user ${socket.id} joined room ${data}`);
+  });
+  socket.on("send-message", (data) => {
+    console.log(data);
+    socket.to(data.room).emit("receive-message", data);
+  });
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+  });
+});
+server.listen(3001, () => {
+  console.log("Server Running");
+});
